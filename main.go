ba7dce0c1581944c977d13c014d9595de0bb7400
AbulAsar Sayyad <abul.a@kiprosh.com>
2020-11-23 19:55:38 +0530

    added contact page

--- main.go
+++ main.go
@@ -7,7 +7,12 @@ import (
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "<h1>Welcome to my awesome website </h1>")
+	if r.URL.Path == "/" {
+		fmt.Fprint(w, "<h1>Welcome to my awesome website </h1>")
+	} else if r.URL.Path == "/contact" {
+		fmt.Fprint(w, "<h3>THis is contact page</h3>")
+	}
+
 }
 
 func main() {
